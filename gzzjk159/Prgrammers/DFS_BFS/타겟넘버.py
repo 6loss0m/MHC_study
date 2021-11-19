b66712394a78978028c69a653bd45b620811a2bb
gzzjk159 <72865221+gzzjk159@users.noreply.github.com>
2021-11-19 18:32:08 +0900

    Create 타겟넘버.py

---
+++ gzzjk159/Prgrammers/DFS_BFS/타겟넘버.py
@@ -0,0 +1,17 @@
+def solution(numbers, target):
+    answer = 0
+    queue = [[numbers[0],0], [-1*numbers[0],0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+#큐에 인덱스와 그 인덱스에서 가질수 있는 값들을 넣어놓고
+#pop을 했을 때 인덱스가 숫자가 맞는지 비교하고 만약 맞을 때 타겟과 비교해서 맞으면 
+#answer+1 해주는 코드이다
