785ec2d303c7b8b5a46802c3021f3b381d399c6e
gzzjk159 <72865221+gzzjk159@users.noreply.github.com>
2022-04-27 21:38:55 +0900

    2022-04-27 할일 끝

---
+++ gzzjk159/BoJ/2022_04_27/10451_permutationcycle.py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(10000)
+from collections import deque
+
+def dfs(v):
+    visited[v] = True
+    for i in adj[v]:
+        if not visited[i]:
+            dfs(i)
+
+t = int(sys.stdin.readline().strip())
+
+for _ in range(t):
+    n = int(sys.stdin.readline().strip())
+    adj = [[] for i in range(n+1)]
+    visited = [False]*(n+1)
+    count = 0
+
+    num_list = list(map(int, sys.stdin.readline().strip().split()))
+    num_list = deque(num_list)
+    num_list.appendleft(0)
+    for i in range(1, n+1):
+        adj[i].append(num_list[i])
+
+    for j in range(1, n+1):
+        if not visited[j]:
+            dfs(j)
+            count +=1
+    print(count)
---
+++ gzzjk159/BoJ/2022_04_27/2178_mazesearch.py
@@ -0,0 +1,26 @@
+import sys
+import collections
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+maze = [list(map(int, ' '.join(input()).split())) for _ in range(n)]
+
+# 이동
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+Q = collections.deque([(0, 0)])
+result = 0
+
+while Q:
+    x, y = Q.popleft()
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if maze[nx][ny] == 1:
+                # 방문
+                maze[nx][ny] = maze[x][y] + 1
+                Q.append((nx, ny))
+
+print(maze[n - 1][m - 1])
