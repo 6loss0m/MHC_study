97ada70c2236888f8d8f7f2ddbf3aa556ea9b283
jsh9057 <jsh9057@skuniv.ac.kr>
2022-06-07 20:54:10 +0900

    add BOJ/ PS_0608

--- JeongSH/BOJ/PS_0601/boj2446_Star.java
+++ JeongSH/BOJ/PS_0601/boj2446_Star.java
@@ -8,7 +8,7 @@ public class boj2446_Star {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuffer sb = new StringBuffer();
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());:
         for(int i=N; i>0; i--){
             for(int j=1; j<=(N-i); j++){ sb.append(" "); }
             for(int k=1; k<=2*i-1; k++){ sb.append("*"); }
