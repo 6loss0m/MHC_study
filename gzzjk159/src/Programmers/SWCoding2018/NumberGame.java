c32480e7d601ca00141b9edd504472ca68624815
gzzjk159 <gzzjk159@gmail.com>
2022-07-14 00:13:03 +0900

    2022-07-14

---
+++ gzzjk159/src/Programmers/SWCoding2018/NumberGame.java
@@ -0,0 +1,24 @@
+package Programmers.SWCoding2018;
+import java.util.*;
+
+public class NumberGame {
+    static class Solution {
+        public int solution(int[] A, int[] B) {
+            int answer = 0;
+            Arrays.sort(A);
+            Arrays.sort(B);
+            int idx_a = A.length - 1;
+            int idx_b = idx_a;
+            while (idx_a >= 0) {
+                int a = A[idx_a];
+                int b = B[idx_b];
+                if (b > a) {
+                    idx_b--;
+                    answer++;
+                }
+                idx_a--;
+            }
+            return answer;
+        }
+    }
+}
