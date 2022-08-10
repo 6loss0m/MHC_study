3b015c376f97f37988bb8e8afc46aba59e264362
gzzjk159 <gzzjk159@gmail.com>
2022-08-10 21:57:33 +0900

    2022-08-10

---
+++ gzzjk159/src/Programmers/MonthlycodeChallenge2/BracketTurn.java
@@ -0,0 +1,46 @@
+package Programmers.MonthlycodeChallenge2;
+import java.util.*;
+
+public class BracketTurn {
+    public int solution(String s) {
+        int answer = 0;
+        String new_s = s;
+        for(int i = 0; i < s.length(); i++){
+            if(checkString(new_s)) answer++;
+            new_s = new_s.substring(1, s.length()) + new_s.charAt(0);
+        }
+
+        return answer;
+    }
+
+    boolean checkString(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            try{
+                switch (s.charAt(i)) {
+                    case '[' -> stack.add('[');
+                    case ']' -> {
+                        if (stack.peek() != '[') return false;
+                        stack.pop();
+                    }
+                    case '{' -> stack.add('{');
+                    case '}' -> {
+                        if (stack.peek() != '{') return false;
+                        stack.pop();
+                    }
+                    case '(' -> stack.add('(');
+                    case ')' -> {
+                        if (stack.peek() != '(') return false;
+                        stack.pop();
+                    }
+                }
+            } catch(Exception e){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ gzzjk159/src/Programmers/MonthlycodeChallenge2/move110.java
@@ -0,0 +1,65 @@
+package Programmers.MonthlycodeChallenge2;
+import java.util.*;
+
+public class move110 {
+    public static String[] solution(String[] s) {
+        String[] answer = new String[s.length];
+        StringBuilder sb;
+
+        for(int i = 0 ; i < s.length; i++){
+            String str = s[i];
+            Stack<Character> stack = new Stack<>();
+            int cnt = 0;
+
+            for(int j = 0 ; j < str.length() ; j++){
+                char z = str.charAt(j);
+
+                if(stack.size() > 1){
+                    char y = stack.pop();
+                    char x = stack.pop();
+
+                    if(x == '1' && y == '1' && z == '0')
+                        cnt++;
+                    else{
+                        stack.push(x);
+                        stack.push(y);
+                        stack.push(z);
+                    }
+                }
+                else
+                    stack.push(z);
+            }
+
+            int idx = stack.size();
+            boolean flag = false;
+            sb = new StringBuilder();
+
+            // 0의 마지막 위치 찾기
+            while(!stack.isEmpty()){
+                if(!flag){
+                    if(stack.peek() == '1')
+                        idx--;
+                    else
+                        flag = true;
+                }
+                sb.insert(0, stack.pop());
+            }
+
+            if(cnt > 0){
+                while(cnt-- > 0){
+                    sb.insert(idx, "110");
+                    idx += 3;
+                }
+                answer[i] = sb.toString();
+            }
+            else
+                // 110 이 없다는 의미이므로 그대로
+                answer[i] = s[i];
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}
