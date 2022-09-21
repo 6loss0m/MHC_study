e144477081ef8b67c05f1586b5a620b82e6978e6
gzzjk <gzzjk159@gmail.com>
2022-09-21 21:26:44 +0900

    2022-09-21

---
+++ gzzjk159/src/Programmers/blindrecruitment_2022/br_2022_0921/disappearscaffolding.java
@@ -0,0 +1,11 @@
+package Programmers.blindrecruitment_2022.br_2022_0921;
+
+public class disappearscaffolding {
+    public int solution(int[][] board, int[][] skill) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+    }
+}
